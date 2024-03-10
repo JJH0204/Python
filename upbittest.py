5abd9aac13389eea2ec9809aaf23a45d33b14520
JJH0204 <jaeho6627@kakao.com>
2024-03-10 19:58:07 +0900

    python test

--- upbittest.py
+++ upbittest.py
@@ -1,12 +1,15 @@
 # UPbit test code
 import pyupbit
+import os
 
 # datafram type
-# df = pyupbit.get_ohlcv("KRW-BTC", count=7)
-# print(df)
+df = pyupbit.get_ohlcv("KRW-BTC", count=200)
+print(df)
 
 # print(pyupbit.get_tickers(fiat="KRW"))
 
-print(pyupbit.get_current_price("KRW-BTC"))
+# print(pyupbit.get_current_price("KRW-BTC"))
 
-print(pyupbit.get_current_price(["KRW-BTC", "KRW-ETH"]))
+# print(pyupbit.get_current_price(["KRW-BTC", "KRW-ETH"]))
+
+os.system('pause')
