9970a1aa31cea30575fad4c5710b375c12eeb41f
JJH0204 <jaeho6627@kakao.com>
2024-04-09 22:45:28 +0900

    Update Ticketing

--- project/AutoTicketing/python/py_interparkTicketing.py
+++ project/AutoTicketing/python/py_interparkTicketing.py
@@ -5,63 +5,79 @@ from selenium.webdriver.common.keys import Keys  # 키보드 키를 사용하기
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
 
 import json
 import urllib.parse
 
 import os
 import time
-
-def initDriver():
-    driver_path = 'project/AutoTicketing/chromedriver.exe'
-    service = Service(executable_path=driver_path)
-    driver = webdriver.Chrome(service=service)
-    return driver
+import easyocr
 
 def readJson():
     with open('project/AutoTicketing/login_config.json', 'r') as file:        # json 로그인 정보 가져오기
         config = json.load(file)
     return config
 
-def loginInterpark(driver, config):
+def initDriver(config):
+    # 브라우저 꺼짐 방지
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+
+    # 크롬 드라이버 위치 설정
+    driver_path = config['driver_path']
+    service = Service(executable_path=driver_path)
+
+    # 드라이버 생성
+    driver = webdriver.Chrome(options=chrome_options, service=service)
+
+    # 브라우저 사이즈
+    driver.set_window_size(config['window_size']['width'], config['window_size']['length'])
+
+    # 웹페이지가 로드될 때까지 2초를 대기
+    driver.implicitly_wait(time_to_wait=2)  
+
+    # 인터파크 접속
+    driver.get(url=config['search_Site'])
+    return driver
+
+def loginInterpark(driver, pageLodeWait, config):
     # Step-2: 로그인
-    login_link = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//ul[contains(@class, 'header_myMenu__h7hl8')]//a[@href='javascript:;']")))
+    login_link =pageLodeWait.until(EC.presence_of_element_located((By.XPATH, "//ul[contains(@class, 'header_myMenu__h7hl8')]//a[@href='javascript:;']")))
     login_link.click()
 
-    login_type = config['login_type']       # json에 정의한 로그인 정보를 가져와 로그인 시도
+    # 웹페이지가 로드될 때까지 2초를 대기
+    driver.implicitly_wait(time_to_wait=2)  
+
+    # json에 정의한 로그인 정보를 가져와 로그인 시도
+    login_type = config['login_type']       
 
     # Step-2.1: 일반 로그인
     if login_type == "local":
-        username_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "userId")))
-        password_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "userPwd")))
+        username_input = pageLodeWait.until(EC.presence_of_element_located((By.ID, "userId")))
+        password_input = pageLodeWait.until(EC.presence_of_element_located((By.ID, "userPwd")))
 
         username_input.send_keys(config['credentials']['id'])
         password_input.send_keys(config['credentials']['password'])
-        # 로그인 버튼이 클릭 가능할 때까지 대기
-        try:
-            login_button = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.ID, "btn_login"))
-            )
-            # 로그인 버튼 클릭
-            login_button.click()
-        except TimeoutException:
-            print("로그인 버튼이 클릭 가능한 상태가 되지 않았습니다.")
+        password_input.send_keys(Keys.ENTER)
 
     # Step-2.2: SNS 로그인 (카카오)
     elif login_type == "kakao":
-        kakaoLogin_link = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "kakao")))
+        kakaoLogin_link = pageLodeWait.until(EC.presence_of_element_located((By.CLASS_NAME, "kakao")))
         kakaoLogin_link.click()
         
-        username_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "loginId--1")))
+        username_field = pageLodeWait.until(EC.presence_of_element_located((By.ID, "loginId--1")))
         username_field.send_keys(config['social']['kakao']['id'])
         
-        password_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "password--2")))
+        password_field = pageLodeWait.until(EC.presence_of_element_located((By.ID, "password--2")))
         password_field.send_keys(config['social']['kakao']['password'])
         
-        kakaoLogin_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button.btn_g.highlight.submit")))
+        kakaoLogin_button = pageLodeWait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "button.btn_g.highlight.submit")))
         kakaoLogin_button.click()
 
-        continue_button = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button.btn_agree")))
+        continue_button = pageLodeWait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "button.btn_agree")))
         continue_button.click()
 
     # Step-2.3: SNS 로그인 (네이버)
@@ -73,8 +89,13 @@ def loginInterpark(driver, config):
     # elif login_type == "apple":
     #     appleLogin_link = driver.find_element(By.CLASS_NAME, "apple")
     #     appleLogin_link.click()
+    
+    # 웹페이지가 로드될 때까지 2초를 대기
+    driver.implicitly_wait(time_to_wait=2)
+    
+    return
 
-def searchTicket(driver, config):
+def searchTicket(driver, pageLodeWait, config):
     # Step-3: 공연 검색
     search_query = config['search_Key']
     search_Type = config['search_Type']
@@ -84,69 +105,122 @@ def searchTicket(driver, config):
         url = f"https://isearch.interpark.com/result?q={encoded_query}"
         driver.get(url)
         # 위 방법은 완벽하지 않아 추후에 추가 작업이 발생할 수 있다.
-    # TODO: 검색 필드를 활용한 공연 검색
     elif search_Type == "search_field":
         # 검색 입력 필드 선택하여 팝업 활성화
-        search_field_main = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "inputSearch")))
+        search_field_main = pageLodeWait.until(EC.element_to_be_clickable((By.ID, "inputSearch")))
         search_field_main.click()
 
         # 팝업 내 검색 입력 필드가 활성화될 때까지 대기
-        search_field_popup = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "input_autoKeyword")))
+        search_field_popup = pageLodeWait.until(EC.element_to_be_clickable((By.ID, "input_autoKeyword")))
 
 		# 검색 입력 필드에 검색어 입력
         search_field_popup.send_keys(search_query)
         search_field_popup.send_keys(Keys.ENTER)
 
+    # 웹페이지가 로드될 때까지 2초를 대기
+    driver.implicitly_wait(time_to_wait=2)
+
     # 원하는 검색어가 포함된 링크를 찾는다.
-    links = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "a[data-prd-name*='"+search_query+"']")))
+    links = pageLodeWait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "a[data-prd-name^='"+search_query+"']")))
+
+    # 웹페이지가 로드될 때까지 2초를 대기
+    driver.implicitly_wait(time_to_wait=2)
 
     # 첫 번째 링크를 선택하여 클릭하거나 새 탭에서 열기
     if links:
         link = links[0].get_attribute('href')
         print(f"Found link: {link}")
         # 새 탭에서 링크 열기
-        # driver.execute_script("window.open(arguments[0]);", link)
-        # 또는 클릭하여 현재 창에서 링크 열기
-        links[0].click()
-        # 현재 탭(원래 탭) 저장
-        # original_window = driver.current_window_handle
-        
-        # 새 탭으로 전환하기
-        # new_window = [window for window in driver.window_handles if window != original_window][0]
-        # driver.switch_to.window(new_window)
+        driver.execute_script("window.open(arguments[0]);", link)
     else:
         print("Link not found.")
+    
+    # 웹페이지가 로드될 때까지 2초를 대기
+    driver.implicitly_wait(time_to_wait=2)
 
+    print(driver.window_handles)
+    driver.switch_to.window(driver.window_handles[-1])
 
-def ticketingInterpark(driver, config):
-    # 안내 팝업이 뜨면 에러가 발생한다.
-    # TODO: 안내 팝업 닫기
-    # 팝업 닫기 버튼을 찾아서 클릭
-    try:
-        close_button = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "popupCloseBtn is-bottomBtn")) # CSS 선택자를 사용하여 팝업의 '닫기'버튼을 찾는다.
-        )
-        close_button.click()
-    except Exception as e:
-        print("팝업을 닫을 수 없습니다.", e)
+    # 웹페이지가 로드될 때까지 2초를 대기
+    driver.implicitly_wait(time_to_wait=5)
 
-    # 이후 예매하기 등의 다른 작업을 계속 진행합니다.
-    os.system('pause')
+    return
 
-    driver.quit()
+# 좌석 탐색
+def select(driver):
+    print(driver.window_handles)
+    driver.switch_to.window(driver.window_handles[-1])
+    driver.switch_to.frame(driver.find_element(By.XPATH,'//*[@id="ifrmSeat"]'))
+    
+    # 좌석등급 선택
+    #driver.find_element(By.XPATH,'//*[@id="GradeRow"]/td[1]/div/span[2]').click()
+    
+    while True:
+        # 세부 구역 선택
+        driver.find_element(By.XPATH,'//*[@id="GradeDetail"]/div/ul/li[1]/a').click()
+        
+        # 좌석선택 아이프레임으로 이동
+        driver.switch_to.frame(driver.find_element(By.XPATH,'//*[@id="ifrmSeatDetail"]'))
+        
+        # 좌석이 있으면 좌석 선택
+        try:
+            driver.find_element(By.XPATH,'//*[@id="Seats"]').click()
+            # 결제 함수 실행
+            # 좌석선택 완료 버튼 클릭
+            driver.switch_to.default_content()
+            driver.switch_to.frame(driver.find_element(By.XPATH,'//*[@id="ifrmSeat"]'))
+            driver.find_element(By.XPATH,'//*[@id="NextStepImage"]').click()
+            break
+            
+        # 좌석이 없으면 다시 조회
+        except:
+            print('******************************다시선택')
+            driver.switch_to.default_content()
+            driver.switch_to.frame(driver.find_element(By.XPATH,'//*[@id="ifrmSeat"]'))
+            driver.find_element(By.XPATH,'/html/body/form[1]/div/div[1]/div[3]/div/p/a/img').click()
+            time.sleep(1)
+    return
+
+def ticketingInterpark(driver, pageLodeWait):
+    # 안내 팝업 닫기
+    popup_button = driver.find_element(By.XPATH, '//*[@id="popup-prdGuide"]/div/div[3]/div/a')
+    popup_button.click()
+
+    # 예매 버튼 누르기
+    ticketing_button = driver.find_element(By.XPATH, '//*[@id="productSide"]/div/div[2]/a[1]')
+    ticketing_button.click()
+
+    # 웹페이지가 로드될 때까지 2초를 대기
+    driver.implicitly_wait(time_to_wait=2)
+
+    # 예매 창으로 이동
+    driver.switch_to.window(driver.window_handles[-1])
+
+    # reCAPTCHA 해결까지 대기
+    time.sleep(300/5)
+
+    select(driver)
+    return
 
 def ticketingProcessInterpark():
     # Step-0: Json파일 로드 및 웹 드라이버 실행
-    config = readJson()
-    driver = initDriver()
     # Step-1: 인터파크 접속
-    driver.get('https://www.interpark.com/')
+    config = readJson()
+    driver = initDriver(config)
+    pageLodeWait = WebDriverWait(driver, 30)    # 페이지 로딩 대기 시간
+    
     # Step-2: 로그인
-    loginInterpark(driver, config)
+    loginInterpark(driver, pageLodeWait, config)
+
     # Step-3: 공연 검색
-    searchTicket(driver, config)
+    searchTicket(driver, pageLodeWait, config)
+
     # Step-4: 공연 예매
-    ticketingInterpark(driver, config)
+    ticketingInterpark(driver, pageLodeWait)
+
+    driver.quit()
+
+    return
 
 # 이 파이썬 파일을 직접 실행할 때 실행될 main()
 if __name__ == "__main__":
---
+++ project/LearnPy/markdown/usingPylance.md
@@ -0,0 +1,2 @@
+# 파이썬 자동완성 기능 사용
+[(링크)](https://newstroyblog.tistory.com/283)
